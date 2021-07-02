dabd361d4894658dd9401445e0c6f0c657121fa0
Carrington Rain Allison <Carrington_Allison@yahoo.com>
2021-07-02 14:42:52 -0700

    Build(Database): Configure Mongo Database
    Add dotenv dependencyy. Set up database connection in server.js.

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+
 //import express from the express library that was installed via NPM 
 const express = require('express');
 //set app variable by calling funciton of express
 const app = express();
 //grab the express layouts package
 const expressLayouts = require('express-ejs-layouts');
-
 //require the Router that I added on routes/index.js
 const indexRouter = require('./routes/index.js')
+
 
 //set the view engine
 app.set('view engine', 'ejs');
@@ -17,7 +22,19 @@ app.set('layout', 'layouts/layout');
 // tell express we want to use express layouts
 app.use(expressLayouts);
 //tell express where public files are 
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+//database setup 
+// require mongoose
+const mongoose = require('mongoose');
+//set up connection with url saved in environment variable
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+//access the connection and set to db variable
+const db = mongoose.connection
+//display the error if received
+db.on('error', err => console.error(err));
+//.once tells it to run once and "open" means when it first opens connection
+db.once('open', () => console.log('Connected to Mongoose DB'))
 
 //call the reference to the Router 
 app.use('/', indexRouter);
